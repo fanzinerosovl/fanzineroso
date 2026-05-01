248e7421e97b3829362fd835feee0e255301a033
lncgomz <lncgomz@gmail.com>
2026-05-01 12:58:03 -0500

    Add fixed code

--- src/app/services/download.service.ts
+++ src/app/services/download.service.ts
@@ -4,13 +4,29 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class DownloadService {
+  private getBasePath(): string {
+    let path = window.location.pathname;
+
+    if (!path.endsWith('/')) {
+      const lastSegment = path.split('/').pop() ?? '';
+      path = lastSegment.includes('.') ? path.replace(/\/[^/]*$/, '/') : `${path}/`;
+    }
+
+    return path;
+  }
+
+  private buildAssetUrl(relativePath: string): string {
+    const normalizedPath = relativePath.replace(/^\/+/, '');
+    return `${window.location.origin}${this.getBasePath()}${normalizedPath}`;
+  }
+
   /**
    * Downloads the digital version of a fanzine
    * @param volume The volume identifier (e.g., "1", "2", etc.)
    */
   downloadDigital(volume: string): void {
     const link = document.createElement('a');
-    link.href = `fanzines/Fanzineroso_${volume}.pdf`;
+    link.href = this.buildAssetUrl(`fanzines/Fanzineroso_${volume}.pdf`);
     link.download = `Fanzineroso_${volume}-digital.pdf`;
     link.click();
   }
@@ -21,7 +37,7 @@ export class DownloadService {
    */
   downloadPrint(volume: string): void {
     const link = document.createElement('a');
-    link.href = `impresion/Fanzineroso_${volume}.pdf`;
+    link.href = this.buildAssetUrl(`impresion/Fanzineroso_${volume}.pdf`);
     link.download = `Fanzineroso_${volume}-print.pdf`;
     link.click();
   }
@@ -32,8 +48,6 @@ export class DownloadService {
    * @returns The full URL to the digital version of the fanzine
    */
   getDownloadUrl(volume: string): string {
-    const baseUrl = window.location.origin;
-    const basePath = window.location.pathname.replace(/\/[^/]*$/, '');
-    return `${baseUrl}${basePath}/fanzines/Fanzineroso_${volume}.pdf`;
+    return this.buildAssetUrl(`fanzines/Fanzineroso_${volume}.pdf`);
   }
 }
